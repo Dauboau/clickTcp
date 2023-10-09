5c9b3e4c1dfc0f8f86b765bb5bdd4b8036650be4
Dauboau <86164187+Dauboau@users.noreply.github.com>
2023-10-09 11:37:01 -0300

    Start Screen Beta

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import threading
+from kivy.uix.popup import Popup 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen,NoTransition
 from kivy.properties import ObjectProperty
 from kivy.properties import NumericProperty
 import random
@@ -33,14 +36,66 @@ class PlayerButton(Button):
     
     num_clicks = NumericProperty(0)
 
-class StartWindow(GridLayout):
+class StartWindow(Screen):
     def __init__(self, **kwargs):
-        Builder.load_file("start.kv")
         super(StartWindow, self).__init__(**kwargs)
 
-class MainWindow(FloatLayout):
+    def host(self):
+        #print("host")
+
+        layout = GridLayout(cols = 1, padding = 10) 
+  
+        portInput = TextInput(hint_text='port',input_filter = 'int',multiline=False,halign = 'center',font_size=80,background_color=(0,0,0,0))
+        closeButton = Button(text = "Confirmar")
+  
+        layout.add_widget(portInput) 
+        layout.add_widget(closeButton)        
+  
+        popup = Popup(title ='Host - Defina uma porta!', content = layout,size_hint=(None, None), size=(800, 400))
+
+        popup.open()
+
+        def confirma(obj):
+
+            try:
+                port = int(portInput.text)
+            except:
+                self.alert('Digite o valor de uma porta! Dica: 54545')
+                return
+
+            print(portInput.text)
+
+            if(port>0 and port<65535):
+                self.manager.current = 'MainWindow'
+                popup.dismiss()
+
+            else:
+                self.alert('O valor da porta deve ser entre 0 e 65535!')
+  
+        closeButton.bind(on_press = confirma)
+
+
+    def client(self):
+        print("client")
+
+    def alert(self,text_alert):
+
+        layoutAlert = GridLayout(cols = 1, padding = 10) 
+        closeAlertButton = Button(text = "Ok")
+        layoutAlert.add_widget(closeAlertButton) 
+
+        alert = Popup(title = text_alert,content=layoutAlert,size_hint=(None, None), size=(800, 250))
+        alert.open()
+
+        closeAlertButton.bind(on_press = alert.dismiss)
+    
+
+class MainWindow(Screen):
     def __init__(self, **kwargs):
         super(MainWindow, self).__init__(**kwargs)
+
+        return 
+    
         player_button = ObjectProperty(None)
         player_label = ObjectProperty(None)
         self.player_label.set_pos(self.x, self.y)
@@ -67,10 +122,15 @@ class MainWindow(FloatLayout):
                 
             #print(p2_data)
 
+class Manager(ScreenManager):
+    screen_one = ObjectProperty(None)
+    screen_two = ObjectProperty(None)
+
 class MyApp(App):
     def build(self):
-        self.title = 'ClickTCP'
-        return StartWindow()
+        #self.title = 'ClickTCP'
+        Builder.load_file("screen.kv")
+        return Manager(transition=NoTransition())
 
 if __name__ == '__main__':
     MyApp().run()
